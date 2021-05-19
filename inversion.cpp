37ecfd1e7510ec8bf93ab25e4cf9271050f378c2
Yannik Könneker <y.koenneker@web.de>
2021-05-19 20:52:08 +0200

    fix broken openmp

--- inversion.cpp
+++ inversion.cpp
@@ -1,103 +1,90 @@
 #include <stdio.h>
+#include <iostream>
 #include <omp.h>
 
 #define minvalue 0.0000
 
-/*template<size_t rows, size_t columns>
-void single_cpu(float (&matrix)[rows][columns], float (&iden)[irows][icolumns], int dimension) {
-	float temporary, r;
-	int j, i, k, temp;
-	
-	for (j = 0; j < dimension; j++) {
-		temp = j;
-		
-		finding maximum jth column element in last (dimension-j) rows
-		
-		for (i = j + 1; i < dimension; i++)
-			if (matrix[i][j] > matrix[temp][j])
-				temp = i;
-		
-		if (fabs(matrix[temp][j]) < minvalue) {
-			printf("\n Elements are too small to deal with");
-			break;
+template<int dim>
+void single_cpu(float matrix[dim][dim], float iden[dim][dim]) {
+	for (int i = 0; i < dim; i++) {
+		if (matrix[i][i] == 0) { // swap lines if 0
+			for (int j = i + 1; j < dim; j++) { // find new line
+				if (matrix[j][i] != 0) {
+					int swap;
+					for (int y = i; y < dim; y++) { // swap lines
+						swap = matrix[i][y];
+						matrix[i][y] = matrix[j][y];
+						matrix[j][y] = swap;
+						swap = iden[i][y];
+						iden[i][y] = iden[j][y];
+						iden[j][y] = swap;
+					}
+					break;
+				}
+			}
 		}
 		
-		swapping row which has maximum jth column element
-		
-		if (temp != j)
-			for (k = 0; k < 2 * dimension; k++) {
-				temporary = matrix[j][k];
-				matrix[j][k] = matrix[temp][k];
-				matrix[temp][k] = temporary;
-			}
-		
-		performing row operations to form required identity matrix out of the input matrix
-		for (i = 0; i < dimension; i++)
-			if (i != j) {
-				r = matrix[i][j];
-				for (k = 0; k < 2 * dimension; k++)
-					matrix[i][k] -= (matrix[j][k] / matrix[j][j]) * r;
-			} else {
-				r = matrix[i][j];
-				for (k = 0; k < 2 * dimension; k++)
-					matrix[i][k] /= r;
-			}
-		
-	}
-}*/
-
-
-template<size_t rows, size_t columns>
-void openmp_offload(float (&matrix)[rows][columns], float (&iden)[rows][columns], int dimension) {
-	for (int i = 0; i < dimension; i++) {
 		//normalize
-		if (matrix[i][i] > 0) {
-#pragma omp target teams distribute parallel for private(i)
-			for (int y = i; y < dimension; y++) {
-				iden[i][y] /= matrix[i][i];
-				matrix[i][y] /= matrix[i][i];
-			}
-		} else {
-#pragma omp target teams distribute parallel for private(i)
-			for (int y = i; y < dimension; y++) {
-				iden[i][y] /= -matrix[i][i];
-				matrix[i][y] /= -matrix[i][i];
+		float factor = matrix[i][i];
+		for (int x = 0; x < dim; x++) {
+			iden[i][x] /= factor;
+			matrix[i][x] /= factor;
+		}
+		for (int y = 0; y < dim; y++) {
+			factor = matrix[y][i];
+			if (y != i && factor != 0.0f) {
+				for (int x = 0; x < dim; x++) {
+					matrix[y][x] -= matrix[i][x] * factor;
+					iden[y][x] -= iden[i][x] * factor;
+				}
 			}
 		}
-		
-		/*/gauß
-#pragma omp target teams distribute parallel for collapse(2) private(i)
-		for (int x = 0; x < dimension; x++) {
-			for (int y = 0; y < dimension; y++) {
-				if (x != i) {
-					iden[x][y] -= iden[i][y] * matrix[x][i];
-					if (y != i) {
-						matrix[x][y] -= matrix[i][y] * matrix[x][i];
+	}
+}
+
+
+template<int dim>
+void openmp_offload(float matrix[dim][dim], float iden[dim][dim]) {
+//#pragma omp target
+	{
+		for (int i = 0; i < dim; i++) {
+			if (matrix[i][i] == 0) { // swap lines if 0
+				for (int j = i + 1; j < dim; j++) { // find new line
+					if (matrix[j][i] != 0) {
+						int swap;
+#pragma omp target teams distribute parallel for private(swap)
+						for (int y = i; y < dim; y++) { // swap lines
+							swap = matrix[i][y];
+							matrix[i][y] = matrix[j][y];
+							matrix[j][y] = swap;
+							swap = iden[i][y];
+							iden[i][y] = iden[j][y];
+							iden[j][y] = swap;
+						}
+						break;
 					}
 				}
 			}
-		}*/
-		int temp;
-#pragma omp target teams distribute parallel for shared(i) private(temp)
-		for (int x = 0; x < dimension; x++) {
-			temp = matrix[x][i];
-			if (x != i && temp != 0) {
-				for (int y = i; y < dimension; y++) {
-					matrix[x][y] -= matrix[i][y] * temp;
-					iden[x][y] -= matrix[i][y] * temp;
+			
+			//normalize
+			float factor = matrix[i][i];
+#pragma omp target teams distribute parallel for
+			for (int x = 0; x < dim; x++) {
+				iden[i][x] /= factor;
+				matrix[i][x] /= factor;
+			}
+//#pragma omp target teams distribute parallel for shared(matrix, iden, i)
+			for (int y = 0; y < dim; y++) {
+				factor = matrix[y][i];
+				if (y != i && factor != 0.0f) {
+					for (int x = 0; x < dim; x++) {
+						matrix[y][x] -= matrix[i][x] * factor;
+						iden[y][x] -= iden[i][x] * factor;
+					}
 				}
 			}
 		}
-		
-		/*set 0
-#pragma omp target teams distribute parallel for collapse(2) private(i)
-		for (int x = 0; x < dimension; x++)
-			for (int y = 0; y < dimension; y++)
-				if (x != i)
-					if (y == i)
-						matrix[x][y] = 0;*/
 	}
-#pragma omp target exit data map(from: iden[dimension][dimension])
 }
 
 /*template<size_t rows, size_t columns>
--- main.cpp
+++ main.cpp
@@ -8,16 +8,17 @@
 #include <Eigen/Dense>
 #include <chrono>
 #include <string>
-
+#include <map>
+#include <fmt/format.h>
 const int dimension = 1000;
 float matrix[dimension][dimension];
 float identity_matrix[dimension][dimension];
 
+
 void matrix_read() {
 	int row = 0;
 	
-	std::ifstream infile("randomMatrix_" + std::to_string(dimension) + ".txt");
-	
+	std::ifstream infile(("randomMatrix_" + std::to_string(dimension) + ".txt").c_str());
 	for (std::string line; std::getline(infile, line);) {
 		std::istringstream inputline(line);
 		for (int i = 0; i < dimension; i++) {
@@ -29,10 +30,9 @@ void matrix_read() {
 }
 
 int main() {
-	std::cout << "INVERSE OF NON-SINGULAR MATRIX BY GAUSS-JORDAN ELIMINATION METHOD" << std::endl;
-	
 	matrix_read();
-
+	
+	//fill identity matrix
 #pragma omp parallel for collapse(2)
 	for (int i = 0; i < dimension; i++) {
 		for (int j = 0; j < dimension; j++) {
@@ -44,60 +44,70 @@ int main() {
 		}
 	}
 	
-	float eigen[dimension][dimension];
+	/*auto eigen = new float[dimension][dimension];
 	std::copy(&matrix[0][0], &matrix[0][0] + dimension * dimension, &eigen[0][0]);
-	auto eigenM = Eigen::Map<Eigen::Matrix<float, dimension, dimension, Eigen::RowMajor>>(&eigen[0][0]);
+	auto eigenM = new Eigen::Map<Eigen::Matrix<float, dimension, dimension, Eigen::RowMajor>>(&eigen[0][0], dimension, dimension);
 	
 	auto start = std::chrono::high_resolution_clock::now();
-	auto eigenresult = eigenM.inverse();
+	auto eigenresult = eigenM->inverse();
 	auto end = std::chrono::high_resolution_clock::now();
 	
-	std::chrono::duration<double> eigen_time = end - start;
-	std::cout << "Eigen Time - inverse:\n" << eigen_time.count() << "ms" << std::endl;
+	std::chrono::duration<float> eigen_time = end - start;
+	std::cout << "Eigen Time - inverse:\n" << eigen_time.count() << "ms" << std::endl;*/
 	
 	
-	/*start = std::chrono::high_resolution_clock::now();
-	single_cpu(matrix, identity_matrix, dimension);
-	end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> cpu_time = end - start;
-	std::cout << "CPU Time - inverse:\n" << cpu_time.count() << "ms" << std::endl;*/
+	auto cpu = new float[dimension][dimension];
+	std::copy(&matrix[0][0], &matrix[0][0] + dimension * dimension, &cpu[0][0]);
+	auto cpures = new float[dimension][dimension];
+	std::copy(&identity_matrix[0][0], &identity_matrix[0][0] + dimension * dimension, &cpures[0][0]);
+	
+	auto start = std::chrono::high_resolution_clock::now();
+	single_cpu(cpu, cpures);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> cpu_time = end - start;
+	std::cout << "CPU Time - inverse:\n" << cpu_time.count() << "ms" << std::endl;
 	
 	
-	float openmp[dimension][dimension];
+	auto openmp = new float[dimension][dimension];
 	std::copy(&matrix[0][0], &matrix[0][0] + dimension * dimension, &openmp[0][0]);
-	float openmpres[dimension][dimension];
-	std::copy(&matrix[0][0], &matrix[0][0] + dimension * dimension, &openmpres[0][0]);
+	auto openmpres = new float[dimension][dimension];
+	std::copy(&identity_matrix[0][0], &identity_matrix[0][0] + dimension * dimension, &openmpres[0][0]);
 #pragma omp target enter data map(to: openmp[0:dimension][0:dimension], openmpres[0:dimension][0:dimension])
 	
 	start = std::chrono::high_resolution_clock::now();
-	openmp_offload(openmp, openmpres, dimension);
+	openmp_offload(openmp, openmpres);
 	end = std::chrono::high_resolution_clock::now();
 
-#pragma omp target exit data map(from: openmpres[dimension][dimension])
-	std::chrono::duration<double> openmp_offload_time = end - start;
+#pragma omp target exit data map(from: openmpres[0:dimension][0:dimension])
+	std::chrono::duration<float> openmp_offload_time = end - start;
 	std::cout << "OpenMP Offload Time - inverse:\n" << openmp_offload_time.count() << "ms" << std::endl;
 	
 	
 	/*start = std::chrono::high_resolution_clock::now();
 	openacc_offload(matrix, identity_matrix, dimension);
 	end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> openacc_offload_time = end - start;
+	std::chrono::duration<float> openacc_offload_time = end - start;
 	std::cout << "OpenACC Offload Time - inverse:\n" << openacc_offload_time.count() << "s" << std::endl;*/
 	
 	/*start = std::chrono::high_resolution_clock::now();
 	cuda(&matrix, dimension);
 	end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> cuda_time = end - start;
+	std::chrono::duration<float> cuda_time = end - start;
 	std::cout << "CUDA Time - inverse:\n" << cuda_time << "ms" << std::endl;*/
 	
-	/*for (int i = 0; i < dimension; i++) {
+	
+	float threshold = 0.00001;
+	int errc = 0;
+	for (int i = 0; i < dimension; i++) {
 		for (int j = 0; j < dimension; j++) {
-			std::cout << eigenresult(i, j) - openmpres[i][j] << " ";
+			if (abs(cpures[i][j] - openmpres[i][j]) > threshold) {
+				errc++;
+				//std::cout << "Error at " << i << " " << j << std::endl;
+				//std::cout << eigenresult(i, j) << " vs " << openmpres[i][j] << std::endl;
+			}
 		}
-		std::cout << std::endl;
-	}*/
-	
-	std::cout << eigenresult(0) << std::endl;
-	
+	}
+	std::cout << "errc: " << errc << std::endl;
+	std::cout << cpures[1][1] << " vs " << openmpres[1][1] << std::endl;
 	return 0;
 }
